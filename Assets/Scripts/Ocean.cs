903595c35cc07238609dd72a35e119ef3e69e5ed
Mozobo <61582099+Mozobo@users.noreply.github.com>
2023-09-05 22:52:37 +0200

    Gaussian noise texture generation

--- Assets/Scripts/Ocean.cs
+++ Assets/Scripts/Ocean.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+﻿using UnityEditor;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Ocean : MonoBehaviour
@@ -10,6 +11,8 @@ public class Ocean : MonoBehaviour
     private Vector3[] vertices;
     private int[] triangles;
     private Mesh mesh;
+    private Texture2D randomNoiseTexture;
+    private const string texturesPath = "Assets/Textures/";
 
     private void GenerateVertices(){
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
@@ -45,8 +48,53 @@ public class Ocean : MonoBehaviour
         GenerateVertices();
         GenerateTriangles();
     }
+
+    // Generates a random number from a Normal Distribution N(0, 1)
+    // Extracted from: https://www.alanzucconi.com/2015/09/16/how-to-sample-from-a-gaussian-distribution/
+    private float GenerateRandomNumber(){
+            float v1, v2, s;
+            do {
+                v1 = 2.0f * Random.Range(0f,1f) - 1.0f;
+                v2 = 2.0f * Random.Range(0f,1f) - 1.0f;
+                s = v1 * v1 + v2 * v2;
+            } while (s >= 1.0f || s == 0f);
+            s = Mathf.Sqrt((-2.0f * Mathf.Log(s)) / s);
+        
+            return v1 * s;
+    }
+
+    private Texture2D GenerateRandomNoiseTexture(){
+        Texture2D noiseTexture = new Texture2D(xSize, zSize, TextureFormat.RGFloat, false, true);
+
+        noiseTexture.filterMode = FilterMode.Point;
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < zSize; j++)
+            {
+                noiseTexture.SetPixel(i, j, new Vector4(GenerateRandomNumber(), GenerateRandomNumber()));
+            }
+        }
+        noiseTexture.Apply();
+
+        #if UNITY_EDITOR
+            string filename = "RandomNoiseTexture" + xSize.ToString() + "x" + zSize.ToString()+ ".asset";
+            AssetDatabase.CreateAsset(noiseTexture, texturesPath + filename);
+        #endif
+
+        return noiseTexture;
+    }
+
+    private void GetRandomNoiseTexture(){
+        string filename = "RandomNoiseTexture" + xSize.ToString() + "x" + zSize.ToString() + ".asset";
+        #if UNITY_EDITOR
+            Texture2D noiseTexture = (Texture2D)AssetDatabase.LoadAssetAtPath(texturesPath + filename, typeof(Texture2D));
+        #endif
+        randomNoiseTexture = noiseTexture ? noiseTexture : GenerateRandomNoiseTexture();
+    }
+
     void Awake(){
         Generate();
+        GetRandomNoiseTexture();
     }
 
     // Uncomment this function to visualize vertices
--- Assets/Shaders/Waves.shader
+++ Assets/Shaders/Waves.shader
@@ -44,41 +44,45 @@ Shader "Custom/Waves"
         float3 GerstnerWave (float4 wave, float3 p, inout float3 tangent, inout float3 binormal) {
 		    float steepness = wave.z;
 		    float wavelength = wave.w;
-		    float k = 2 * PI / wavelength;
-			float c = sqrt(_Gravity / k);
-			float2 d = normalize(wave.xy);
-			float f = k * (dot(d, p.xz) - c * _Time.y);
-			float a = steepness / k;
+		    float frequency = 2 * PI / wavelength;
+			float speed = sqrt(_Gravity / frequency);
+			float2 direction = normalize(wave.xy);
+			float f = frequency * (dot(direction, p.xz) - speed * _Time.y);
+			float amplitude = steepness / frequency;
 
 			tangent += float3(
-				-d.x * d.x * (steepness * sin(f)),
-				d.x * (steepness * cos(f)),
-				-d.x * d.y * (steepness * sin(f))
+				-direction.x * direction.x * (steepness * sin(f)),
+				direction.x * (steepness * cos(f)),
+				-direction.x * direction.y * (steepness * sin(f))
 			);
 			binormal += float3(
-				-d.x * d.y * (steepness * sin(f)),
-				d.y * (steepness * cos(f)),
-				-d.y * d.y * (steepness * sin(f))
+				-direction.x * direction.y * (steepness * sin(f)),
+				direction.y * (steepness * cos(f)),
+				-direction.y * direction.y * (steepness * sin(f))
 			);
 
 			return float3(
-				d.x * a * cos(f),
-				a * sin(f),
-				d.y * a * cos(f)
+				direction.x * amplitude * cos(f),
+				amplitude * exp(sin(f) - 1),
+				direction.y * amplitude * cos(f)
 			);
+            
 		}
 
+
+        float3 calculateNormal(float3 tangent, float3 binormal) {
+            return normalize(cross(binormal, tangent));
+        }
+
         void vert(inout appdata_full vertexData) {
-            float3 gridPoint = vertexData.vertex;
+            float3 p = vertexData.vertex;
 			float3 tangent = float3(1, 0, 0);
 			float3 binormal = float3(0, 0, 1);
-			float3 p = gridPoint;
-			p += GerstnerWave(_WaveA, gridPoint, tangent, binormal);
-			p += GerstnerWave(_WaveB, gridPoint, tangent, binormal);
-            p += GerstnerWave(_WaveC, gridPoint, tangent, binormal);
-			float3 normal = normalize(cross(binormal, tangent));
+			p += GerstnerWave(_WaveA, p, tangent, binormal);
+			p += GerstnerWave(_WaveB, p, tangent, binormal);
+            p += GerstnerWave(_WaveC, p, tangent, binormal);
 			vertexData.vertex.xyz = p;
-			vertexData.normal = normal;
+			vertexData.normal = calculateNormal(tangent, binormal);
         }
 
         void surf (Input IN, inout SurfaceOutputStandard o)
