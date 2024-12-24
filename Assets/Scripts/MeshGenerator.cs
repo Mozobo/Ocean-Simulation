90bab623e9e9f9fe1dcfb946ead564ff2a002111
Mozobo <61582099+Mozobo@users.noreply.github.com>
2024-12-24 18:33:08 +0100

    Add mesh generation documentation

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+// This script generates a procedural mesh plane
+// Based on https://catlikecoding.com/unity/tutorials/procedural-grid/
+
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class MeshGenerator : MonoBehaviour
 {
-    [SerializeField, Range(1, 10000)]
-    public int planeSize = 1000;
-    // Reduces the number of vertices used to create a mesh of given size
-    [SerializeField, Range(1, 100)]
-    public int trianglesSize = 10;
+    // Length of one side of the mesh in Unity units, forming a plane of size planeSize x planeSize
+    [SerializeField, Range(1, 100000)] public int planeSize = 1000;
+    // Controls the resolution of the mesh by adjusting the size of the triangles generated
+    [SerializeField, Range(1, 1000)] public int trianglesSize = 10; 
     private Mesh mesh;
 
+    // Generates vertices for the mesh, centering the plane at the GameObject's position
     private void GenerateVertices(){
         int verticesPerRow = planeSize / trianglesSize;
         float halfLength = planeSize * 0.5f;
+        // Spacing between vertices
         float spacing = planeSize / (float)verticesPerRow;
 
         Vector3[] vertices = new Vector3[(verticesPerRow + 1) * (verticesPerRow + 1)];
 
 		for (int i = 0, z = 0; z <= verticesPerRow; z++) {
 			for (int x = 0; x <= verticesPerRow; x++, i++) {
+                // Calculate vertex position in object space
 				vertices[i] = new Vector3((float)x * spacing - halfLength, 0, (float)z * spacing - halfLength);
 			}
 		}
@@ -29,6 +34,7 @@ public class MeshGenerator : MonoBehaviour
         mesh.vertices = vertices;
     }
 
+    // Generates triangles for the mesh using two triangles per grid cell
     private void GenerateTriangles(){
         int verticesPerRow = planeSize / trianglesSize;
         
@@ -49,7 +55,7 @@ public class MeshGenerator : MonoBehaviour
     void Awake(){
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
 		mesh.name = "Procedural Water plane";
-        // This is needed to generate a plane with more than 65.536 vertices
+        // This is needed to generate a plane with more than 65.536 vertices, allowing up to 2^32 vertices
         mesh.indexFormat = IndexFormat.UInt32;
 
         GenerateVertices();
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Ocean-Simulation
+
+Real-time rendering of realistic ocean-like water surfaces using the Inverse Fast Fourier Transform (IFFT), in combination with the Joint North Sea Wave Project (JONSWAP) spectrum and the Texel-Marsen-Arsloe (TMA) modification, within Unity's Universal Render Pipeline (URP).
+
+## Table of contents
+
+- [Mesh generation](#mesh-generation)
+- [Ocean spectrum](#ocean-spectrum)
+- [IFFT](#ifft)
+- [Shader](#shader)
+- [Buoyancy](#buoyancy)
+- [How to use it](#how-to-use-it)
+- [Coming next](#coming-next)
+- [References](#references)
+
+## Mesh generation
+
+The first step is to generate a mesh that forms the base of the water body. In this project, a low-resolution plane is created. The idea is to increase the number of vertices in the areas close to the camera using a tesselation shader. The code in ```MeshGenerator.cs``` is based on [Catlike Coding's Procedural Grid tutorial](https://catlikecoding.com/unity/tutorials/procedural-grid/), but adjusted to have the plane centered at the GameObject's position and to allow the modification of the triangles's size.
+
+> [!TIP]
+> For big planes I recommend keeping the triangles's size between 25 and 100 . Below 25 will be too resource consuming due to the shader running for each tesselated triangle and above 100 will probably not have enough resolution even with the tesselation.
+
+> [!IMPORTANT]  
+> The plane is created at the start of the execution and modifying plane size and triangle size at run-time will not change it. Only changes in position, rotation and scale will be reflected.
+
+
+## Ocean spectrum
+## IFFT
+## Shader
+## Buoyancy
+## How to use it
+## Coming next
+## References
+
