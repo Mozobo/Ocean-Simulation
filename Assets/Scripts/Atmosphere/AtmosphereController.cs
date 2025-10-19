d85a9614944438cb7845ca4dc9284397ca7bb79d
Mozobo <61582099+Mozobo@users.noreply.github.com>
2025-10-19 19:58:38 +0200

    Change sun color based on elevation

--- Assets/Scripts/Atmosphere/AtmosphereController.cs
+++ Assets/Scripts/Atmosphere/AtmosphereController.cs
@@ -6,6 +6,9 @@ public class AtmosphereController : MonoBehaviour
 {
     public static readonly int SUN_DIRECTION = Shader.PropertyToID("_SunDirection");
 
+    private Light sunLight;
+    public Gradient colorBySunElevation;
+
     public int transmittanceLUTWidth = 64;
     public int transmittanceLUTHeight = 256;
 
@@ -18,21 +21,21 @@ public class AtmosphereController : MonoBehaviour
     public float planetRadius = 6360000;
     public float atmosphereRadius = 6420000;
 
-    public Vector3 rayleighScatteringCoefficient = new Vector3(5.802e-6f, 13.558e-6f, 33.1e-6f);
+    public Vector3 rayleighScatteringCoefficient = new Vector3(5.802e-6f, 13.558e-6f, 6.5e-5f);
     public Vector3 rayleighAbsorptionCoefficient = new Vector3(0f, 0f, 0f);
     public float rayleighScaleHeight = 8000;
 
     public Vector3 mieScatteringCoefficient = new Vector3(3.996e-6f, 3.996e-6f, 3.996e-6f);
     public Vector3 mieAbsorptionCoefficient = new Vector3(4.4e-6f, 4.4e-6f, 4.4e-6f);
     public float mieScaleHeight = 1200;
-    public float g = 0.8f;
+    public float g = 0.85f;
     
     public Vector3 ozoneScatteringCoefficient = new Vector3(0f, 0f, 0f);
     public Vector3 ozoneAbsorptionCoefficient = new Vector3(0.65e-6f, 1.881e-6f, 0.085e-6f);
 
     public Vector3 groundAlbedo = new Vector3(0.0f, 0.0f, 0.0f);
 
-    private RenderTexture transmittanceLUT;
+    public RenderTexture transmittanceLUT;
     private RenderTexture multiscatteringLUT;
     private RenderTexture skyViewLUT;
 
@@ -61,22 +64,29 @@ public class AtmosphereController : MonoBehaviour
         return rt;
     }
 
-    private void InitializeTransmittanceLUTComputeShader() {
+    private void InitializeCommonAtmosphereParams(ComputeShader shader)
+    {
+        shader.SetFloat("_PlanetRadius", planetRadius);
+        shader.SetFloat("_AtmosphereRadius", atmosphereRadius);
+        shader.SetFloat("_MieG", g);
+
+        shader.SetVector("_RayleighScatteringCoefficient", new Vector4(rayleighScatteringCoefficient.x, rayleighScatteringCoefficient.y, rayleighScatteringCoefficient.z));
+        shader.SetVector("_MieScatteringCoefficient", new Vector4(mieScatteringCoefficient.x, mieScatteringCoefficient.y, mieScatteringCoefficient.z));
+        shader.SetVector("_OzoneScatteringCoefficient", new Vector4(ozoneScatteringCoefficient.x, ozoneScatteringCoefficient.y, ozoneScatteringCoefficient.z));
+
+        shader.SetVector("_RayleighAbsorptionCoefficient", new Vector4(rayleighAbsorptionCoefficient.x, rayleighAbsorptionCoefficient.y, rayleighAbsorptionCoefficient.z));
+        shader.SetVector("_MieAbsorptionCoefficient", new Vector4(mieAbsorptionCoefficient.x, mieAbsorptionCoefficient.y, mieAbsorptionCoefficient.z));
+        shader.SetVector("_OzoneAbsorptionCoefficient", new Vector4(ozoneAbsorptionCoefficient.x, ozoneAbsorptionCoefficient.y, ozoneAbsorptionCoefficient.z));
+        
+        shader.SetFloat("_RayleighScaleHeight", rayleighScaleHeight);
+        shader.SetFloat("_MieScaleHeight", mieScaleHeight);
+    }
+
+    private void InitializeTransmittanceLUTComputeShader()
+    {
         transmittanceLUTComputeShader.SetInt("_LutWidth", transmittanceLUTWidth);
         transmittanceLUTComputeShader.SetInt("_LutHeight", transmittanceLUTHeight);
-        transmittanceLUTComputeShader.SetFloat("_PlanetRadius", planetRadius);
-        transmittanceLUTComputeShader.SetFloat("_AtmosphereRadius", atmosphereRadius);
-
-        transmittanceLUTComputeShader.SetVector("_RayleighScatteringCoefficient", new Vector4(rayleighScatteringCoefficient.x, rayleighScatteringCoefficient.y, rayleighScatteringCoefficient.z));
-        transmittanceLUTComputeShader.SetVector("_MieScatteringCoefficient", new Vector4(mieScatteringCoefficient.x, mieScatteringCoefficient.y, mieScatteringCoefficient.z));
-        transmittanceLUTComputeShader.SetVector("_OzoneScatteringCoefficient", new Vector4(ozoneScatteringCoefficient.x, ozoneScatteringCoefficient.y, ozoneScatteringCoefficient.z));
-
-        transmittanceLUTComputeShader.SetVector("_RayleighAbsorptionCoefficient", new Vector4(rayleighAbsorptionCoefficient.x, rayleighAbsorptionCoefficient.y, rayleighAbsorptionCoefficient.z));
-        transmittanceLUTComputeShader.SetVector("_MieAbsorptionCoefficient", new Vector4(mieAbsorptionCoefficient.x, mieAbsorptionCoefficient.y, mieAbsorptionCoefficient.z));
-        transmittanceLUTComputeShader.SetVector("_OzoneAbsorptionCoefficient", new Vector4(ozoneAbsorptionCoefficient.x, ozoneAbsorptionCoefficient.y, ozoneAbsorptionCoefficient.z));
-
-        transmittanceLUTComputeShader.SetFloat("_RayleighScaleHeight", rayleighScaleHeight);
-        transmittanceLUTComputeShader.SetFloat("_MieScaleHeight", mieScaleHeight);
+        InitializeCommonAtmosphereParams(transmittanceLUTComputeShader);
 
         transmittanceLUTComputeShader.SetTexture(KERNEL_TRANSMITTANCE_LUT, "_TransmittanceLUT", transmittanceLUT);
     }
@@ -88,19 +98,7 @@ public class AtmosphereController : MonoBehaviour
     private void InitializeMultiscatteringLUTComputeShader() {
         multiscatteringLUTComputeShader.SetInt("_LutWidth", multiscatteringLUTWidth);
         multiscatteringLUTComputeShader.SetInt("_LutHeight", multiscatteringLUTHeight);
-        multiscatteringLUTComputeShader.SetFloat("_PlanetRadius", planetRadius);
-        multiscatteringLUTComputeShader.SetFloat("_AtmosphereRadius", atmosphereRadius);
-
-        multiscatteringLUTComputeShader.SetVector("_RayleighScatteringCoefficient", new Vector4(rayleighScatteringCoefficient.x, rayleighScatteringCoefficient.y, rayleighScatteringCoefficient.z));
-        multiscatteringLUTComputeShader.SetVector("_MieScatteringCoefficient", new Vector4(mieScatteringCoefficient.x, mieScatteringCoefficient.y, mieScatteringCoefficient.z));
-        multiscatteringLUTComputeShader.SetVector("_OzoneScatteringCoefficient", new Vector4(ozoneScatteringCoefficient.x, ozoneScatteringCoefficient.y, ozoneScatteringCoefficient.z));
-
-        multiscatteringLUTComputeShader.SetVector("_RayleighAbsorptionCoefficient", new Vector4(rayleighAbsorptionCoefficient.x, rayleighAbsorptionCoefficient.y, rayleighAbsorptionCoefficient.z));
-        multiscatteringLUTComputeShader.SetVector("_MieAbsorptionCoefficient", new Vector4(mieAbsorptionCoefficient.x, mieAbsorptionCoefficient.y, mieAbsorptionCoefficient.z));
-        multiscatteringLUTComputeShader.SetVector("_OzoneAbsorptionCoefficient", new Vector4(ozoneAbsorptionCoefficient.x, ozoneAbsorptionCoefficient.y, ozoneAbsorptionCoefficient.z));
-
-        multiscatteringLUTComputeShader.SetFloat("_RayleighScaleHeight", rayleighScaleHeight);
-        multiscatteringLUTComputeShader.SetFloat("_MieScaleHeight", mieScaleHeight);
+        InitializeCommonAtmosphereParams(multiscatteringLUTComputeShader);
 
         multiscatteringLUTComputeShader.SetVector("_GroundSpectrumAlbedo", groundAlbedo);
 
@@ -115,20 +113,7 @@ public class AtmosphereController : MonoBehaviour
     private void InitializeSkyViewLUTComputeShader() {
         skyViewLUTComputeShader.SetInt("_LutWidth", skyViewLUTWidth);
         skyViewLUTComputeShader.SetInt("_LutHeight", skyViewLUTHeight);
-        skyViewLUTComputeShader.SetFloat("_PlanetRadius", planetRadius);
-        skyViewLUTComputeShader.SetFloat("_AtmosphereRadius", atmosphereRadius);
-        skyViewLUTComputeShader.SetFloat("_MieG", g);
-
-        skyViewLUTComputeShader.SetVector("_RayleighScatteringCoefficient", new Vector4(rayleighScatteringCoefficient.x, rayleighScatteringCoefficient.y, rayleighScatteringCoefficient.z));
-        skyViewLUTComputeShader.SetVector("_MieScatteringCoefficient", new Vector4(mieScatteringCoefficient.x, mieScatteringCoefficient.y, mieScatteringCoefficient.z));
-        skyViewLUTComputeShader.SetVector("_OzoneScatteringCoefficient", new Vector4(ozoneScatteringCoefficient.x, ozoneScatteringCoefficient.y, ozoneScatteringCoefficient.z));
-
-        skyViewLUTComputeShader.SetVector("_RayleighAbsorptionCoefficient", new Vector4(rayleighAbsorptionCoefficient.x, rayleighAbsorptionCoefficient.y, rayleighAbsorptionCoefficient.z));
-        skyViewLUTComputeShader.SetVector("_MieAbsorptionCoefficient", new Vector4(mieAbsorptionCoefficient.x, mieAbsorptionCoefficient.y, mieAbsorptionCoefficient.z));
-        skyViewLUTComputeShader.SetVector("_OzoneAbsorptionCoefficient", new Vector4(ozoneAbsorptionCoefficient.x, ozoneAbsorptionCoefficient.y, ozoneAbsorptionCoefficient.z));
-
-        skyViewLUTComputeShader.SetFloat("_RayleighScaleHeight", rayleighScaleHeight);
-        skyViewLUTComputeShader.SetFloat("_MieScaleHeight", mieScaleHeight);
+        InitializeCommonAtmosphereParams(skyViewLUTComputeShader);
 
         skyViewLUTComputeShader.SetVector("_SunDirection", -this.transform.forward);
 
@@ -141,7 +126,36 @@ public class AtmosphereController : MonoBehaviour
         skyViewLUTComputeShader.Dispatch(KERNEL_SKYVIEW_LUT, skyViewLUTWidth/LOCAL_WORK_GROUPS_X, skyViewLUTHeight/LOCAL_WORK_GROUPS_Y, 1);
     }
 
+    private void TurnTransmittanceIntoColorGradient() {
+        RenderTexture.active = transmittanceLUT;
+
+        Texture2D readableTex = new Texture2D(transmittanceLUT.width, transmittanceLUT.height, TextureFormat.RGBA32, false);
+        readableTex.ReadPixels(new Rect(0, 0, transmittanceLUT.width, transmittanceLUT.height), 0, 0);
+        readableTex.Apply();
+
+        RenderTexture.active = null;
+
+        colorBySunElevation = new Gradient();
+
+        const int sampleCount = 8;
+        float[] intervals = new float[] { 0.01f, 0.14f, 0.28f, 0.36f, 0.57f, 0.75f, 0.86f, 0.99f };
+        GradientColorKey[] colorKeys = new GradientColorKey[sampleCount];
+        GradientAlphaKey[] alphaKeys = new GradientAlphaKey[sampleCount];
+
+        for (int i = 0; i < sampleCount; i++)
+        {
+            float t = intervals[i];
+            Color color = readableTex.GetPixelBilinear(0f, t) * 2.5f;
+            colorKeys[i] = new GradientColorKey(color, t);
+            alphaKeys[i] = new GradientAlphaKey(1.0f, t);
+        }
+
+        colorBySunElevation.SetKeys(colorKeys, alphaKeys);
+    }
+
     void Awake() {
+        sunLight = GetComponent<Light>();
+
         transmittanceLUT = CreateRenderTexture(transmittanceLUTWidth, transmittanceLUTHeight, RenderTextureFormat.ARGBFloat, false);
         multiscatteringLUT = CreateRenderTexture(multiscatteringLUTWidth, multiscatteringLUTHeight, RenderTextureFormat.ARGBFloat, false);
         skyViewLUT = CreateRenderTexture(skyViewLUTWidth, skyViewLUTHeight, RenderTextureFormat.ARGBFloat, false);
@@ -153,6 +167,8 @@ public class AtmosphereController : MonoBehaviour
         InitializeTransmittanceLUTComputeShader();
         ComputeTransmittanceLUT();
 
+        TurnTransmittanceIntoColorGradient();
+
         InitializeMultiscatteringLUTComputeShader();
         ComputeMultiscatteringLUT();
 
@@ -160,11 +176,15 @@ public class AtmosphereController : MonoBehaviour
         ComputeSkyViewLUT();
 
         material.SetTexture("_SkyViewLUT", skyViewLUT);
-        material.SetTexture("_TransmittanceLUT", transmittanceLUT);
     }
 
     void Update() {
         skyViewLUTComputeShader.SetVector("_SunDirection", -this.transform.forward);
         ComputeSkyViewLUT();
+
+        // Compute the sun's elevation: 1 on zenith, -1 when below the horizon.
+        // Remap the range [-1, 1] to [0, 1] for sampling the color gradient.
+        float sunElevation = (Vector3.Dot(this.transform.forward, Vector3.down) + 1f) * 0.5f; 
+        sunLight.color = colorBySunElevation.Evaluate(sunElevation);
     }
 }
--- Assets/Shaders/Atmosphere.shader
+++ Assets/Shaders/Atmosphere.shader
@@ -2,7 +2,6 @@ Shader "Custom/Atmosphere"
 {
     Properties {
         _SunSize ("Sun Size", Range(0,1)) = 0.04
-        _SunIntensity("Sun Intensity", Range(0, 10)) = 2
     }
     SubShader {
         Tags { 
@@ -35,12 +34,9 @@ Shader "Custom/Atmosphere"
             };
             
             uniform half _SunSize;
-            uniform half _SunIntensity;
 
             TEXTURE2D(_SkyViewLUT);
             SAMPLER(sampler_SkyViewLUT);
-            TEXTURE2D(_TransmittanceLUT);
-            SAMPLER(sampler_TransmittanceLUT);
 
             float4 SampleSkyLUT(float3 rayDir) {
                 rayDir = normalize(rayDir);
@@ -56,26 +52,14 @@ Shader "Custom/Atmosphere"
                 return SAMPLE_TEXTURE2D(_SkyViewLUT, sampler_SkyViewLUT, uv);
             }
 
-            float4 SampleTransmittanceLUT(float3 sunDir) {
-                sunDir = normalize(sunDir);
-
-                float3 up = float3(0.0, 1.0, 0.0);
-                float sunCosZenithAngle = dot(sunDir, up);
-                float2 uv = float2(
-                    0,
-                    0.5 + 0.5 * sign(sunCosZenithAngle) * sqrt(abs(sunCosZenithAngle) * 2.0/PI)
-                );
-                return SAMPLE_TEXTURE2D(_TransmittanceLUT, sampler_TransmittanceLUT, uv);
-            }
-
             // Calculates the sun shape
             // Code source: https://github.com/TwoTailsGames/Unity-Built-in-Shaders/blob/master/DefaultResourcesExtra/Skybox-Procedural.shader#L346
-            half SunIntensity(half3 lightPos, half3 ray) {
+            half SunShape(half3 lightPos, half3 ray) {
                 if (ray.y <= 0.0) return 0;
                 half3 delta = lightPos - ray;
                 half dist = length(delta);
                 half spot = 1.0 - smoothstep(0.0, _SunSize, dist);
-                return spot * spot * _SunIntensity;
+                return spot * spot;
             }
 
             v2f vert (appdata v)
@@ -90,8 +74,8 @@ Shader "Custom/Atmosphere"
 
             float4 frag (v2f i) : SV_Target
             {
-                float4 sunColor = SunIntensity(_MainLightPosition, i.viewDir) * _MainLightColor * max(0.5, SampleTransmittanceLUT(_MainLightPosition));
-                float4 skyColor = SampleSkyLUT(i.viewDir) * _SunIntensity;
+                float4 sunColor = SunShape(_MainLightPosition, i.viewDir) * _MainLightColor;
+                float4 skyColor = SampleSkyLUT(i.viewDir) * 2;
 
                 return sunColor + skyColor;
             }
